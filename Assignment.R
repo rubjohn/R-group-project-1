d372d92cc6d7c957a9f785716deb67738f2206ca
Chris Best <cbest8@gatech.edu>
2025-03-25 15:03:43 -0400

    worked down to create the decision tree.  Next step is to force an error

--- Assignment.R
+++ Assignment.R
@@ -11,18 +11,25 @@ library(tidyverse)
 
 #import loan information
 loan_data <- read.csv("loan_application.csv")
-
+http://127.0.0.1:29383/graphics/plot_zoom_png?width=722&height=809
 #summarize data
 summary(loan_data)
 
 #determine loan status counts
 summary(as.factor(loan_data$loan_status))
 
+#import and read E1_ind.csv
+E1_ind.csv <-read.csv('E1_ind.csv')$x
 
+#partition dataset into training and testing subsets
+loan_train = loan_data[E1_ind.csv,] 
+loan_test = loan_data[-E1_ind.csv,]
 
-
-
-
+#use Rpart to create decision tree
+loan_status_class <- rpart(loan_status ~ gender + total_income + dti + credit_score + education + employment, method="class", data=loan_train, cp = 0.015)
+rpart.plot(loan_status_class, tweak = 1.2)
+rpart.plot(titanic_tree_class_cp, extra = 104, tweak = 1.2)
+rpart.plot(titanic_tree_class_cp, extra = 1, tweak = 1.2)
 
 
 
